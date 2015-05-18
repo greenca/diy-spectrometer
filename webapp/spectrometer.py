8405712aefb71dcece1a959fc93eec5de0de83b4
green <green@cganalytics.org>
2015-05-17 23:49:17 -0700

    timestamp for image files

--- webapp/spectrometer.py
+++ webapp/spectrometer.py
@@ -1,16 +1,20 @@
-from flask import Flask, url_for
+from flask import Flask, url_for, render_template
 import os
+from datetime import datetime
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    filename = "spectrum_{:s}.jpg".format(datetime.now().isoformat())
     os.system("wmctrl -a ' fps)'")
     os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
-    os.system("gnome-screenshot -w -B -f static/test.jpg")
+    time.sleep(0.1)
+    os.system("gnome-screenshot -w -B -f static/" + filename)
     os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
     os.system("wmctrl -a localhost")
-    return '<img src="' + url_for('static', filename='test.jpg') + '">'
+    return render_template('spectrometer.html', filepath=url_for('static', filename=filename))
 
 if __name__ == '__main__':
     app.debug = True
