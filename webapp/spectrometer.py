6ad619492c8cc1fc7d08913e8fdc9b2811a9716d
green <green@cganalytics.org>
2015-05-24 15:15:48 -0700

    calibration page for webapp

--- webapp/spectrometer.py
+++ webapp/spectrometer.py
@@ -12,15 +12,23 @@ def spectrum():
         description = request.form['description']
         description = description.translate({ord(c): None for c in punctuation + whitespace})
         filename = "spectrum_{:s}_{:s}.png".format(description, datetime.now().isoformat())
-        os.system("wmctrl -a ' fps)'")
-        os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
-        time.sleep(0.1)
-        os.system("import -window root -crop 350x200+1280+480 static/" + filename)
-        os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
-        os.system("wmctrl -a localhost")
-        return render_template('spectrometer.html', filepath=url_for('static', filename=filename))
+        takePicture(filename)
+        return render_template('spectrometer.html', filepath=url_for('static', filename="spectra/" + filename))
     else:
         return render_template('spectrometer.html', filepath=None)
+
+@app.route('/calibrate', methods=['GET', 'POST'])
+def calibration():
+    return render_template('calibration.html', filepath=None)
+
+def takePicture(filename):
+    os.system("wmctrl -a ' fps)'")
+    os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
+    time.sleep(0.1)
+    os.system("import -window root -crop 350x200+1280+480 static/spectra/" + filename)
+    os.system("wmctrl -r ' fps)' -b toggle,fullscreen")
+    os.system("wmctrl -a localhost")
+
 
 if __name__ == '__main__':
     app.debug = True
